bc38035da367eadd8a784064915eb7da85b6d16b
Adam Gaia <git@agaia.dev>
2023-08-20 21:28:46 -0600

    feat: fetchTarball -> fetchGit
    trying out a builtin in hopes we can import default.nix in my NUR repo

--- default.nix
+++ default.nix
@@ -1,9 +1,11 @@
 (import
   (
     let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
-    fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
+    fetchGit {
+      url = "https://github.com/edolstra/flake-compat.git";
+      #url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      rev = lock.nodes.flake-compat.locked.rev;
+      # sha256 = lock.nodes.flake-compat.locked.narHash;
     }
   )
   { src = ./.; }
